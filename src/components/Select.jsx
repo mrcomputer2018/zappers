11ddbb0e80d9580a600b927363d84545340330f3
mrcomputer2018 <45561350+mrcomputer2018@users.noreply.github.com>
2021-09-07 11:59:52 -0300

    componente select

---
+++ src/components/Select.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Select = (props) => {
+    return (
+        <div>
+            <select className="select-sector">
+                <option>Marketing</option>
+                <option>Desenvolvimento de BOT</option>
+                <option>QA/option</option>
+            </select>
+        </div>
+    )
+}
+export default Select;
